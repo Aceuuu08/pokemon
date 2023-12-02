4b3b4dbc9323e8fe829e0c5505ee9423b19e17f6
Aceuuu08 <141605070+Aceuuu08@users.noreply.github.com>
2023-12-02 23:26:20 +0800

    modify admin.py

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import PokemonCard
 # Register your models here.
 admin.site.register(PokemonCard)
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
